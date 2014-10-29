9d2bdffb38f306a7e0785b55a8a485a261bdae4a
Doug Luce <doug@cindy-git.con.com>
2014-10-29 14:02:23 -0700

    Refactor Redis tests.
    Check Redis directly to see whether the key has expired.  This should
    avoid adding fudge factors to the test duration, although fudge is
    still necessary to compensate for Redis' 1-second expiration
    resolution.
    
    Also expire the proper key out of the store.  When the middleware is
    being used, the route key has the remotekey added to it.  Since that's
    not strictly known until a request is made, I've put in the localhost
    IP address as that's what express is giving me for a remote address on
    my box.  No clue if that holds up universally.
    
    Also tried to make the logic a little cleaner, although it's still far
    from perfect.

--- test/common.rate.js
+++ test/common.rate.js
@@ -6,14 +6,15 @@ var request         = require('supertest'),
     rate            = require('../lib/rate'),
     memory          = require('../lib/memory');
 
-var getRouteKey =  function(req) {return module.exports.routeKey};
+var getRouteKey =  function(req) {
+    return module.exports.routeKey
+};
 
 var rand = function(n) {
     return Math.ceil(Math.random() * n);
-}
+};
 
 var performNRequests = function (middleware, n, callback) {
-    
     var app = express();
     var responseBody = 'works';
     
@@ -29,12 +30,20 @@ var performNRequests = function (middleware, n, callback) {
             .expect(responseBody)
             .end(next);
     }, callback);
-}
+};
+
+var checkerr = function(callback) {
+    return function(err, res) { 
+        if (err) throw err;
+        if (typeof callback == 'function')
+            callback();
+    };
+};
 
 
 module.exports = {
     
-    routeKey: 'whoola', // get:/^\\/\\/?$/i',
+    routeKey: 'get:/^\\/\\/?$/i',
     
     'baseline: server can be tested' : function (done) {
 	
@@ -48,7 +57,8 @@ module.exports = {
         
         request(app)
             .get('/')
-            .expect(responseBody, done)
+            .expect(responseBody)
+            .end(checkerr(done))
     },
     
     'server records rate' : function (handler, done) {
@@ -95,13 +105,13 @@ module.exports = {
     },
                       
     'Routes can be rate limited, reallowed, and have proper headers': function (handler, done) {
-        handler.reset(module.exports.routeKey, null, null, function () {
+        handler.reset(module.exports.routeKey, '127.0.0.1', null, function () {
             var app = express();
             var responseBody = 'works';
             var errorJson = 'limited';
             
             var limit = 10;
-            var middleware = rate.middleware({handler: handler, interval: 5, limit: limit, getRouteKey: getRouteKey,
+            var middleware = rate.middleware({handler: handler, interval: 1, limit: limit, getRouteKey: getRouteKey,
             	                              onLimitReached: function (req, res, rate, limit, resetTime, next) {
     		                                  // HTTP code 420 from http://mehack.com/inventing-a-http-response-code-aka-seriously
     		                                  res.status(420).send(errorJson);
@@ -121,7 +131,6 @@ module.exports = {
                 request(app)
                     .get('/')
                     .expect(response.body)
-                    .expect('X-RateLimit-Limit', limit, 'Limit does not match the header limit returned from server.')
                     .expect(function(res) {
                         assert.isDefined(res.headers['X-RateLimit-Limit'.toLowerCase()]);
                         assert.isDefined(res.headers['X-RateLimit-Remaining'.toLowerCase()]);
@@ -129,31 +138,34 @@ module.exports = {
                         
                         // check the headers
                         if (parseInt(res.headers['X-RateLimit-Remaining'.toLowerCase()]) > 0) {
-                            
                             if (!lastCheck) {
                             	makeRequest({body: responseBody});
         	            } else {
                                 done();
-                                return false;
                             }
-                        } else {
+                        } else { // We're being limited.
                             limitedRequests += 1;
                             if (limitedRequests > 5) {
-                             	
-                        	var currentTime = (new Date()).getTime();
-                        	var sleepForMs = parseInt(res.headers['X-RateLimit-Reset'.toLowerCase()]) - currentTime;
-                                // TODO: bug, have to add an extra second for differences between redis time and local time
-                                // need to find correct way to do this
-                        	sleepForMs += 1000;
-                        	setTimeout(function () {
-                        	    lastCheck = true;
-                        	    makeRequest({body: responseBody});
-                        	}, sleepForMs);
+                                var checkTTL = function() { // Wait until the store TTL expires.
+                                    handler.checkttl(getRouteKey(), function(err,resp) {
+                                        if (resp >= 0) { // Still not expired.
+                        	            setTimeout(checkTTL, 50);
+                                        } else {
+                        	            var currentTime = (new Date()).getTime();
+                        	            var expectedExpiryTime = parseInt(res.headers['X-RateLimit-Reset'.toLowerCase()]);
+                                            var redisInaccuracy = 1000; // Redis clock resolution, 1 second.
+                                            assert.operator(currentTime, '>', expectedExpiryTime - redisInaccuracy);
+                        	            lastCheck = true;
+                            	            makeRequest({body: responseBody});
+                                        }
+                                    });
+                                };
+                                checkTTL();
                             } else {
                             	makeRequest({body: errorJson});
                             }
                         }
-                    }).end(function(){});
+                    }).end(checkerr());
             }
             makeRequest({body: responseBody});
         });
--- test/redis.test.js
+++ test/redis.test.js
@@ -4,25 +4,25 @@ var common          = require('./common.rate');
     redis           = require('redis'),
     client          = redis.createClient();
 
-/*
-it('baseline: server can be tested', function (done) {
-  common['baseline: server can be tested'](done);
-});
-
-it('server records rate', function (done) {
- var handler = new rate.Redis.RedisRateHandler({client: client});
- common['server records rate'](handler, done);
-});
-
-
-it('Reset after interval', function (done) {
- var handler = new rate.Redis.RedisRateHandler({client: client});
- common['Reset after interval'](handler, done);
-});
-*/
-
-it('Routes can be rate limited, reallowed, and have proper headers', function (done) {
-    this.timeout(10000);
+describe('Redis-based rate handling', function () {
     var handler = new rate.Redis.RedisRateHandler({client: client});
-    common['Routes can be rate limited, reallowed, and have proper headers'](handler, done);
-});
+
+    it('baseline: server can be tested', function (done) {
+        common['baseline: server can be tested'](done);
+    });
+    it('server records rate', function (done) {
+        common['server records rate'](handler, done);
+    });
+    it('Reset after interval', function (done) {
+        common['Reset after interval'](handler, done);
+    });
+
+    handler.checkttl = function(key, cb) {
+        return client.ttl(key, cb);
+    };
+        
+    it('Routes can be rate limited, reallowed, and have proper headers', function (done) {
+        this.timeout(3000);
+        common['Routes can be rate limited, reallowed, and have proper headers'](handler, done);
+    });
+});    
